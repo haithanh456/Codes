ceaf46c987cc5d61559de2d84563f73cf8d9cdba
haithanh456 <thanh.tran@muivi.com>
2026-07-16 18:55:46 +0700

    Update admin.py

--- Codes/admin.py
+++ Codes/admin.py
@@ -1,23 +1,18 @@
 from django.contrib import admin
 from .models import RedeemCode
 
-
 @admin.register(RedeemCode)
 class RedeemCodeAdmin(admin.ModelAdmin):
-    list_display = ("code", "reward_type", "ball", "special", "currency_amount", 
-                   "expires_at", "current_uses", "max_uses", "is_active")
-    list_filter = ("reward_type", "is_active")
-    search_fields = ("code",)
-    autocomplete_fields = ("ball", "special")
+    list_display = ['code', 'reward_type', 'is_active', 'current_uses', 'max_uses', 'expires_at']
+    list_filter = ['is_active', 'reward_type']
+    search_fields = ['code']
+    list_editable = ['is_active']        # Click to enable/disable
+    actions = ['activate', 'deactivate']
 
-    fieldsets = (
-        (None, {"fields": ("code", "reward_type", "is_active")}),
-        ("Rewards", {"fields": ("ball", "special", "currency_amount")}),
-        ("Limits", {"fields": ("expires_at", "max_uses", "current_uses")}),
-    )
+    def activate(self, request, queryset):
+        queryset.update(is_active=True)
+    activate.short_description = "Activate selected codes"
 
-    def get_queryset(self, request):
-        try:
-            return super().get_queryset(request)
-        except:
-            return RedeemCode.objects.none()
+    def deactivate(self, request, queryset):
+        queryset.update(is_active=False)
+    deactivate.short_description = "Deactivate selected codes"
