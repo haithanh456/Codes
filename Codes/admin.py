b568b2ad1ede348bfcbaf6c4f47c6f31bbf8c00a
haithanh456 <thanh.tran@muivi.com>
2026-07-16 18:15:21 +0700

    Update admin.py

--- Codes/admin.py
+++ Codes/admin.py
@@ -4,31 +4,16 @@ from .models import RedeemCode
 
 @admin.register(RedeemCode)
 class RedeemCodeAdmin(admin.ModelAdmin):
-    list_display = (
-        "code",
-        "reward_type",
-        "ball",
-        "special",
-        "currency_amount",
-        "expires_at",
-        "current_uses",
-        "max_uses",
-        "is_active",
-    )
+    list_display = ("code", "reward_type", "ball", "special", "currency_amount", 
+                   "expires_at", "current_uses", "max_uses", "is_active")
     list_filter = ("reward_type", "is_active")
     search_fields = ("code",)
     autocomplete_fields = ("ball", "special")
 
     fieldsets = (
-        (None, {
-            "fields": ("code", "reward_type", "is_active")
-        }),
-        ("Rewards", {
-            "fields": ("ball", "special", "currency_amount"),
-        }),
-        ("Limits", {
-            "fields": ("expires_at", "max_uses", "current_uses")
-        }),
+        (None, {"fields": ("code", "reward_type", "is_active")}),
+        ("Rewards", {"fields": ("ball", "special", "currency_amount")}),
+        ("Limits", {"fields": ("expires_at", "max_uses", "current_uses")}),
     )
 
     def get_queryset(self, request):
