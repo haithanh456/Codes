7b67f31c648a61385227d464612fe922a17071b3
haithanh456 <thanh.tran@muivi.com>
2026-07-17 17:03:52 +0700

    Update models.py

--- Codes/models.py
+++ Codes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class RedeemCode(models.Model):
     code = models.CharField(max_length=50, unique=True)
-    reward = models.CharField(max_length=100, help_text="Example: Germany or 500 coins")
+    reward = models.CharField(max_length=200, help_text="Example: Germany or 500 coins")
     expires_at = models.DateTimeField(null=True, blank=True)
     max_uses = models.PositiveIntegerField(default=1)
     current_uses = models.PositiveIntegerField(default=0)
