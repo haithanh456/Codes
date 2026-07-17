06003ca2b85234fd627015a12294a3d29193b34e
haithanh456 <thanh.tran@muivi.com>
2026-07-17 17:14:59 +0700

    Update admin.py

--- Codes/admin.py
+++ Codes/admin.py
@@ -3,8 +3,7 @@ from .models import RedeemCode
 
 @admin.register(RedeemCode)
 class RedeemCodeAdmin(admin.ModelAdmin):
-    list_display = ("code", "ball", "special", "expires_at", "current_uses", "max_uses", "is_active")
+    list_display = ("code", "reward", "expires_at", "current_uses", "max_uses", "is_active")
     list_filter = ("is_active",)
-    search_fields = ("code",)
+    search_fields = ("code", "reward")
     list_editable = ("is_active",)
-    autocomplete_fields = ("ball", "special")
