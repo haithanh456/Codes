e0c96e3ff9abc6d423afb5693e94bdd4e400a118
Prodigy Zipzap <thanh.tran@muivi.com>
2026-07-18 17:45:56 +0700

    Update models.py

--- Codes/models.py
+++ Codes/models.py
@@ -1,4 +1,3 @@
-cat > extra/Codes/Codes/models.py << 'EOF'
 from django.db import models
 from bd_models.models import Ball, Special
 
@@ -18,4 +17,3 @@ class RedeemCode(models.Model):
     class Meta:
         verbose_name = "Redeem Code"
         verbose_name_plural = "Redeem Codes"
-EOF
