03916a38e05205f0b70b9b9f5fb872878c0b24b7
Prodigy Zipzap <thanh.tran@muivi.com>
2026-07-18 18:06:16 +0700

    Update admin.py

--- Codes/admin.py
+++ Codes/admin.py
@@ -3,20 +3,7 @@ from .models import RedeemCode
 
 @admin.register(RedeemCode)
 class RedeemCodeAdmin(admin.ModelAdmin):
-    list_display = ("code", "ball", "special", "currency_amount", "expires_at", "current_uses", "max_uses", "is_active")
+    list_display = ("code", "reward", "is_active")
     list_filter = ("is_active",)
-    search_fields = ("code",)
+    search_fields = ("code", "reward")
     list_editable = ("is_active",)
-    raw_id_fields = ("ball", "special")   
-
-    fieldsets = (
-        (None, {
-            "fields": ("code", "is_active")
-        }),
-        ("Reward", {
-            "fields": ("ball", "special", "currency_amount"),
-        }),
-        ("Limits", {
-            "fields": ("expires_at", "max_uses", "current_uses")
-        }),
-    )
