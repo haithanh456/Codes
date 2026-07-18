79ebffcc8b7fde57919f434a4496184fac258694
Prodigy Zipzap <thanh.tran@muivi.com>
2026-07-18 18:06:03 +0700

    Update models.py

--- Codes/models.py
+++ Codes/models.py
@@ -1,14 +1,8 @@
 from django.db import models
-from bd_models.models import Ball, Special
 
 class RedeemCode(models.Model):
     code = models.CharField(max_length=50, unique=True)
-    ball = models.ForeignKey(Ball, on_delete=models.SET_NULL, null=True, blank=True)
-    special = models.ForeignKey(Special, on_delete=models.SET_NULL, null=True, blank=True)
-    currency_amount = models.PositiveIntegerField(default=0)
-    expires_at = models.DateTimeField(null=True, blank=True)
-    max_uses = models.PositiveIntegerField(default=1)
-    current_uses = models.PositiveIntegerField(default=0)
+    reward = models.CharField(max_length=200)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
